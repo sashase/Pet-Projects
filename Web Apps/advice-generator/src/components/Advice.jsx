ef8530ba737ec3517ac542facbf462deae2cab0f
sashase <semenchuksasha21@gmail.com>
2023-01-12 14:49:54 +0200

    little-improvements

--- Web Apps/advice-generator/src/components/Advice.jsx
+++ Web Apps/advice-generator/src/components/Advice.jsx
@@ -1,21 +1,25 @@
 import dice from "../assets/icon-dice.svg"
 import dividerD from "../assets/pattern-divider-desktop.svg"
+import dividerM from "../assets/pattern-divider-mobile.svg"
 
 export default function Advice(props) {
   return (
-    <div className="sm:px-10 lg:px-0">
-      <div className="bg-darkGrayBlue sm:w-full lg:w-1/4 lg:mx-auto my-72 p-10 pb-0 rounded-xl">
+    <div className="px-10 lg:px-0">
+      <div className="bg-darkGrayBlue lg:max-w-lg p-10 pb-0 rounded-xl">
         <div>
-          <h2 className="lg:text-xs sm:text-lg text-primaryGreen uppercase tracking-widest">
+          <h2 className="lg:text-xs text-lg text-primaryGreen uppercase tracking-widest">
             Advice #{props.id}
           </h2>
-          <p className="lg:text-xl sm:text-5xl pt-8 pb-10">{props.slip}</p>
+          <p className="lg:text-xl text-5xl pt-8 pb-10">{props.slip}</p>
         </div>
+        <picture>
+        <source srcSet={dividerM} media = "(max-width: 600px)" />
         <img src={dividerD} alt="" className="lg:py-2 sm:pt-6 mx-auto" />
+        </picture>
         <button
           onClick={props.onClick}
-          className="bg-primaryGreen lg:p-4 sm:p-12 rounded-full relative lg:top-7 sm:top-20 m-0 cursor-pointer">
-          <img src={dice} alt="dice-button" className="sm:w-16 lg:w-full" />
+          className="bg-primaryGreen lg:p-4 p-12 rounded-full relative lg:top-7 top-20 m-0 cursor-pointer">
+          <img src={dice} alt="dice-button" className="w-16 lg:w-full" />
         </button>
       </div>
     </div>
